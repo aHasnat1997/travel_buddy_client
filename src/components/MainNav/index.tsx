3973da1d371424e81c6fbbfd6c98749c54563092
aHasnat1997 <hasnat56703@gmail.com>
2024-05-31 00:01:58 +0600

    ADD: nav sticky

--- src/components/MainNav/index.tsx
+++ src/components/MainNav/index.tsx
@@ -1,11 +1,27 @@
+'use client';
+
 import assets from "@/assets";
 import { Box, Container, Stack } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import NavButton from "./NavButton";
 import NavDrawer from "./NavDrawer";
+import { useEffect, useState } from "react";
 
 function MainNav() {
+  // making sticky nav after scroll 
+  const [sticky, setSticky] = useState<boolean>(false);
+  useEffect(() => {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY > 720) {
+        setSticky(true);
+      }
+      else {
+        setSticky(false);
+      }
+    })
+  }, [])
+
   type TNavContent = {
     title: string;
     path: string;
@@ -62,14 +78,14 @@ function MainNav() {
   </Container>);
 
   return (
-    <>
+    <nav className={`bg-white shadow-xl duration-1000 ${sticky ? 'top-0 fixed right-0 left-0 z-40' : '-top-20'}`}>
       <section className="hidden lg:block">
         <FullScreenView />
       </section>
       <section className="lg:hidden">
         <NavDrawer navContent={navContent} />
       </section>
-    </>
+    </nav>
   );
 }
 
